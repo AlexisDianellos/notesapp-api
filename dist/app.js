226b5fdb31a460ee37b938540e522a1c3f82b5c5
Alexandros Dianellos <alexisdianellos@gmail.com>
2024-09-03 04:00:19 +0300

    a

--- dist/app.js
+++ dist/app.js
@@ -50,6 +50,8 @@ app.use((0, express_session_1.default)({
     saveUninitialized: false,
     cookie: {
         maxAge: 60 * 60 * 1000,
+        sameSite: 'none',
+        httpOnly: true,
     },
     rolling: true,
     store: connect_mongo_1.default.create({
